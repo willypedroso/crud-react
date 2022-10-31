b111b5f2a9dbf05c1edb09ab137a9b646a13d57f
Will Pedroso <willyng.pedroso@gmail.com>
2022-10-31 04:05:42 -0300

    config app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,80 @@
-import { useState } from 'react'
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "chakra-ui/react";
+
+import { useEffect, useState } from "react";
+import ModalComp from "./components/ModalComp";
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [data, setData] = useState([]);
+  const [dataEdit, setDataEdit] = useState({});
 
   return (
-    <h1>hello</h1>
-  )
-}
+    <Flex
+      h="100vh"
+      align="center"
+      justify="center"
+      fontSize="20px"
+      fontFamilly="poppind"
+    >
+      <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+        <Button colorScheme="blue" onClick={() => [setDataEdit({}), onOpen()]}>
+          NEW REGISTER
+        </Button>
+        <Box overflowY="auto" height="100%">
+          <Table mt="6">
+            <Thead>
+              <Tr>
+                <Th maxW={isMobile ? 5: 100} fontSize="20px">
+                  Name
+                </Th>
+                <Th maxW={isMobile ? 5: 100} fontSize="20px">
+                  E-mail
+                </Th>
+                <Th p={0}></Th>
+                <Th p={0}></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.map(({ name, email }, index) => (
+                <Tr key={index} cursor="pointer" _hover={{ bg: "gray.100" }}>
+                  <Td maxW={isMobile ? 5 : 100}>{name}</Td>
+                  <Td maxW={isMobile ? 5 : 100}>{email}</Td>
+                  <Td p={0}>
+                    <EditIcon
+                      fontSize={20}
+                      onClick={() => [
+                        setDataEdit({name, email, index}),
+                        onOpen(),
+                      ]}
+                    />
+                  </Td>
+                  <Td p={0}>
+                    <DeleteIcon
+                      fontSize="20"
+                      onClick={() => handleRemove(email)}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
 
-export default App
+export default App;
